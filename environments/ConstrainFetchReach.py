6d5244be0f92c349d6f7a9678729c68fef3e128e
matthewcool <matthewcool@snu.ac.kr>
2019-11-18 13:29:19 +0900

    Added Gaussian distribution sampler

--- environments/ConstrainFetchReach.py
+++ environments/ConstrainFetchReach.py
@@ -12,26 +12,30 @@ class ConstrainFetchReach(FetchReachEnv):
 
     environment_name = "Fetch-Reach-Constrained-v1"
 
-    def __init__(self, reward_type = 'sparse', constrain = False, initial_info = None):
+    def __init__(self, reward_type = 'sparse', constrain = None, initial_info = None):
         #first initialize the super class
         super().__init__(reward_type)
+        assert constrain in {None, 'Fix', 'Gaussian'}, "variable constrain is not right"
+
         if constrain:
             self.initial_state_1 = initial_info["initial_state"]
             self.goal = initial_info["goal"]
-        self.constrian = constrain
+        self.constrain = constrain
         self.initial_info = initial_info
 
     def reset(self):
 
         self.initial_state_1 = self.initial_info["initial_state"]
-        # self.initial_state =
 
-        if self.constrian:
+        if not self.constrain == None:
             super(RobotEnv, self).reset()
             did_reset_sim = False
             while not did_reset_sim:
                 did_reset_sim = self._reset_sim()
-            self.goal = self.initial_info["goal"]
+            if self.constrain == 'Fix':
+                self.goal = self.initial_info["goal"]
+            elif self.constrain == 'Gaussian':
+                self.goal = [np.random.normal(i, 1) for i in self.initial_info["goal"]]
             obs = self._get_obs()
             return obs
         else:
@@ -59,7 +63,7 @@ if __name__ == "__main__":
             break
 
     print("-----------------------------")
-    env_2 = ConstrainFetchReach(constrain = True, initial_info = initial_info_1)
+    env_2 = ConstrainFetchReach(constrain = 'Fix', initial_info = initial_info_1)
     info = env_2.reset()
     trigger = True
     for i in range(100):
@@ -74,6 +78,22 @@ if __name__ == "__main__":
             print("In attempt ", i)
             trigger = False
             break
+
+    print("-----------------------------")
+    env_3 = ConstrainFetchReach(constrain = 'Gaussian', initial_info = initial_info_1)
+    info = env_3.reset()
+    trigger = True
+    for i in range(100):
+        info_1 = info
+        info = env_3.reset()
+        check1 = np.array_equal(info["achieved_goal"], info_1["achieved_goal"])
+        check2 = np.array_equal(info["observation"], info_1["observation"])
+        check3 = np.array_equal(info["desired_goal"], info_1["desired_goal"])
+        # if not (check1 and check2 and check3):
+        #     print("The \"ConstrainedFetchReach_Gaussian\" is different!!")
+        #     print("The different part: observation: {}, achieved_goal: {}, desired_goal: {}".format(check2, check1, check3))
+        #     print("In attempt ", i)
+        #     trigger = False
 
     if(trigger):
         print("The \"ConstrainedFetchReach\" is Same!!")
