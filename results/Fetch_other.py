e46c4017bc839a6151cdd07dcc9d89b42dbe2ccb
matthewcool <matthewcool@snu.ac.kr>
2019-11-19 16:49:15 +0900

    Added my Constrained Envs to the Time wrapper to end the episode

--- results/Fetch_other.py
+++ results/Fetch_other.py
@@ -8,17 +8,47 @@ from agents.actor_critic_agents.DDPG import DDPG
 from agents.actor_critic_agents.DDPG_HER_Che import DDPG_HER_Che
 from agents.actor_critic_agents.DDPG_HER import DDPG_HER
 from utilities.data_structures.Config import Config
+from gym.wrappers.time_limit import TimeLimit
 from agents.Trainer import Trainer
+
+from environments.ConstrainFetchPush import ConstrainFetchPush
+from environments.ConstrainFetchReach import ConstrainFetchReach
+from environments.ConstrainFetchSlide import ConstrainFetchSlide
+from environments.ConstrainFetchPickAndPlace import ConstrainFetchPickAndPlace
+
+constrain_encode = [None, "Fix", "Gaussian"]
+env_code = ["FetchReach-v1", "FetchPush-v1", "FetchSlide-v1", "FetchPickAndPlace-v1"]
+envclass = [ConstrainFetchReach, ConstrainFetchPush, ConstrainFetchSlide, ConstrainFetchPickAndPlace]
+
+print("Starting the training>>")
+print("What kind of Env would you like to train? (1: Reach, 2: Push, 3: Slide, 4: PickAndPlace): ", end="")
+env_choice = int(input()) - 1
+print("Do you want to make constrain? (0: original, 1: No, 2: Fixed, 3: Gaussian): ", end="")
+constrain_choice = int(input()) - 1
+max_step = 50
+if not constrain_choice == -1:
+    print("Do you want to change the maximum steps of the environment? (No: 0, Yes: positive number): ", end ="")
+    max_step = int(input())
+    if(max_step == 0):
+        max_step = 50
+print("How many episodes per one run?: ", end="")
+episodes_to_run = int(input())
+print("How many runs per agent?: ", end="")
+runs_per_agent = int(input())
 
 
 config = Config()
 config.seed = 1
-config.environment = gym.make("FetchPush-v1")
-#config.environment = gym.make("FetchPush-v1")
-# config.environment = gym.make("FetchPickAndPlace-v1")
-#config.environment = gym.make("FetchSlide-v1")
-config.num_episodes_to_run = 5
-#config.num_episodes_to_run = 2000
+
+
+env = gym.make(env_code[env_choice])
+info = env.reset()
+initial_info = {"initial_state": info["observation"], "goal": info["desired_goal"]}
+env_not_wrapped = envclass[env_choice](constrain=constrain_encode[constrain_choice], initial_info=initial_info)
+config.environment = TimeLimit(env_not_wrapped, max_step)
+if constrain_choice == -1:
+    config.environment = gym.make(env_code[env_choice])
+config.num_episodes_to_run = episodes_to_run
 config.file_to_save_data_results = None
 # config.file_to_save_results_graph = 'data_and_graphs/FetchPush_detecting_Anamaly.png'
 config.file_to_save_results_graph = None
@@ -26,8 +56,7 @@ config.show_solution_score = False
 config.visualise_individual_results = False
 config.visualise_overall_agent_results = True
 config.standard_deviation_results = 1.0
-#config.runs_per_agent = 3
-config.runs_per_agent = 10
+config.runs_per_agent = runs_per_agent
 config.use_GPU = False
 config.overwrite_existing_results_file = False
 config.randomise_random_seed = True
@@ -39,6 +68,7 @@ config.hyperparameters = {
 "Actor_Critic_Agents": {
     "Actor": {
         "learning_rate": 0.001,
+        # "learning_rate": 0.1,
         "linear_hidden_units": [50, 50],
         "final_layer_activation": "TANH",
         "batch_norm": False,
@@ -48,6 +78,7 @@ config.hyperparameters = {
 
     "Critic": {
         "learning_rate": 0.01,
+        # "learning_rate": 0.1,
         "linear_hidden_units": [50, 50, 50],
         "final_layer_activation": None,
         "batch_norm": False,
@@ -70,7 +101,7 @@ config.hyperparameters = {
 
 if __name__== '__main__':
     #AGENTS = [DDPG_HER_Che]
-    AGENTS = [DDPG_HER_Che, DDPG_HER]
+    AGENTS = [DDPG, DDPG_HER_Che, DDPG_HER]
     for i in range(10):
         trainer = Trainer(config, AGENTS)
         trainer.run_games_for_agents()
